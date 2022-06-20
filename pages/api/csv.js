f76d02aec5d3c1631c484dbe364a8f2b71cad79f
popay97 <5880963+popay97@users.noreply.github.com>
2022-06-20 09:13:09 +0200

    ..

--- pages/api/csv.js
+++ pages/api/csv.js
@@ -1,8 +1,29 @@
+import {model,Schema} from 'mongoose';
+import ReservationSchema from '../../models/ReservationModel'
 
 export default function handler(req, res) {
     const data = req.body;
-    let objForSave = {
+    let objForSave = {};
+    for(let i = 0;i<data.length;i++){
+        const found = ReservationSchema.findOne({resId: data[i][0]})
+        if(!found){
+            let phone;
+            if(data[i][4].startsWith("00")){
+                phone = "+" + data[i][4].slice(2)
+            }
+            else{
+                phone = "+" + data[i][4]
+            }
 
+            objforSave ={
+                resId: data[i][0],
+                title: data[i][1],
+                name: data[i][2],
+                surname: data[i][3],
+                phone: phone
+
+            }
+        }
     }
     res.status(200).json({ name: 'John Doe' })
   }
