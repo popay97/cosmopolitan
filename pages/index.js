18c716f6deb498585b8a0063f88fea145d7aaccc
popay97 <5880963+popay97@users.noreply.github.com>
2022-06-24 19:41:52 +0200

    ..

--- pages/index.js
+++ pages/index.js
@@ -1,279 +1,283 @@
-import Head from 'next/head'
-import FileComponent from '../components/FileUpload';
-import dbConnect from '../lib/dbConnect';
-import NDayReport from '../components/NDayReport';
-
+import Head from "next/head";
+import FileComponent from "../components/FileUpload";
+import dbConnect from "../lib/dbConnect";
+import NDayReport from "../components/NDayReport";
 
 export default function Home({ isConnected }) {
-  const [view,setView] = useState("home");
+  const [view, setView] = useState("home");
 
-  if(view === "home"){
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  const changeView = (view) => {
+    setView(view);
+  }
+  if (view === "home") {
+    return (
+      <div className="container">
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main>
-        {isConnected ? (
-          <h2 className="subtitle">You are connected to MongoDB</h2>
-        ) : (
-          <h2 className="subtitle">
-            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
-            for instructions.
-          </h2>
-        )}
-        <div className="grid">
-          <div className="card">
-            <FileComponent />
+        <main>
+          {isConnected ? (
+            <h2 className="subtitle">You are connected to MongoDB</h2>
+          ) : (
+            <h2 className="subtitle">
+              You are NOT connected to MongoDB. Check the <code>README.md</code>{" "}
+              for instructions.
+            </h2>
+          )}
+          <div className="grid">
+            <div className="card">
+              <FileComponent />
+            </div>
+
+            <div className="card" onClick={()={
+              changeView("report")
+            }}>
+              <h3>4-day report &rarr;</h3>
+              <p>Učitaj četvorodnevni report.</p>
+            </div>
+
+            <div className="card">
+              <h3>7-day report &rarr;</h3>
+              <p>Učitaj sedmodnevni report</p>
+            </div>
+
+            <div className="card">
+              <h3>Pretraga &rarr;</h3>
+              <p>Pretraži bazu.</p>
+            </div>
           </div>
+        </main>
 
-          <div  className="card">
-            <h3>4-day report &rarr;</h3>
-            <p>Učitaj četvorodnevni report.</p>
+        <footer>
+          <div className="footer-div">
+            <p>Powered by</p>
+            <img src="/trid-logo.jpg" alt="Trid Logo" className="logo" />
           </div>
+        </footer>
 
-          <div
-            className="card"
-          >
-            <h3>7-day report &rarr;</h3>
-            <p>Učitaj sedmodnevni report</p>
-          </div>
+        <style jsx>{`
+          .container {
+            min-height: 100vh;
+            padding: 0 0.5rem;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
 
-          <div
-            className="card"
-          >
-            <h3>Pretraga &rarr;</h3>
-            <p>
-              Pretraži bazu.
-            </p>
-          </div>
-        </div>
-      </main>
+          main {
+            padding: 5rem 0;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
 
-      <footer>
-        <div className="footer-div" >
-          <p>Powered by</p>
-          <img src="/trid-logo.jpg" alt="Trid Logo" className="logo" />
-        </div>
-      </footer>
+          footer {
+            width: 100%;
+            height: 100px;
+            border-top: 1px solid #eaeaea;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+          .footer-div {
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+          }
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+          a {
+            color: inherit;
+            text-decoration: none;
+          }
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+          .title a {
+            color: #0070f3;
+            text-decoration: none;
+          }
 
-        .footer-div {
-          display:flex;
-          flex-direction: row;
-          justify-content: center;
-          align-items: center;
-          
-        }
+          .title a:hover,
+          .title a:focus,
+          .title a:active {
+            text-decoration: underline;
+          }
 
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
+          .title {
+            margin: 0;
+            line-height: 1.15;
+            font-size: 4rem;
+          }
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
+          .title,
+          .description {
+            text-align: center;
+          }
 
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
+          .subtitle {
+            font-size: 2rem;
+          }
 
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
+          .description {
+            line-height: 1.5;
+            font-size: 1.5rem;
+          }
 
-        .title,
-        .description {
-          text-align: center;
-        }
+          code {
+            background: #fafafa;
+            border-radius: 5px;
+            padding: 0.75rem;
+            font-size: 1.1rem;
+            font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+              DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+          }
 
-        .subtitle {
-          font-size: 2rem;
-        }
+          .grid {
+            display: flex;
+            align-items: flex-start;
+            justify-content: center;
+            flex-wrap: wrap;
+            max-width: 800px;
+            margin-top: 3rem;
+          }
 
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
+          .card {
+            margin: 1rem;
+            flex-basis: 45%;
+            padding: 1.5rem;
+            text-align: left;
+            color: inherit;
+            text-decoration: none;
+            border: 1px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+          }
 
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
+          .card:hover,
+          .card:focus,
+          .card:active {
+            border: 4px solid #000000;
+            transition: 0.5s;
+          }
 
-        .grid {
-          display: flex;
-          align-items: flex-start;
-          justify-content: center;
-          flex-wrap: wrap;
-          max-width: 800px;
-          margin-top: 3rem;
-        }
+          .card h3 {
+            margin: 0 0 1rem 0;
+            font-size: 1.5rem;
+          }
 
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
+          .card p {
+            margin: 0;
+            font-size: 1.25rem;
+            line-height: 1.5;
+          }
 
-        .card:hover,
-        .card:focus,
-        .card:active {
-          border: 4px solid #000000;
-          transition: 0.5s;
-
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
+          .logo {
             width: 3.5rem;
             margin-left: 20px;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
           }
-        }
-      `}</style>
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-        }
+          @media (max-width: 600px) {
+            .grid {
+              width: 100%;
+              flex-direction: column;
+            }
+          }
+        `}</style>
 
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-    )
-  }
-  else if(view === "report"){
-  return(
-  <div className="container">
-    <Head>  <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <main>
-      <div className="table">
+        <style jsx global>{`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+          }
 
-        </div>
-    </main>
-    <footer>  
-      <div className="footer-div" >
-        <p>Powered by</p>
-        <img src="/trid-logo.jpg" alt="Trid Logo" className="logo" />
+          * {
+            box-sizing: border-box;
+          }
+        `}</style>
       </div>
-    </footer>
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-      
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;  
-        justify-content: center;
-        align-items: center;
-      }
-      .footer-div {
-        display:flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: center;
-        
-      }`}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-   ) 
-}
-else{
+    );
+  } else if (view === "report") {
     return (
-      <div>kurcina</div> )
-}
+      <div className="container">
+        <Head>
+          {" "}
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main>
+          <div className="table">
+            <NDayReport />
+          </div>
+        </main>
+        <footer>
+          <div className="footer-div">
+            <p>Powered by</p>
+            <img src="/trid-logo.jpg" alt="Trid Logo" className="logo" />
+          </div>
+        </footer>
+        <style jsx>{`
+          .container {
+            min-height: 100vh;
+            padding: 0 0.5rem;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+
+          main {
+            padding: 5rem 0;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+          footer {
+            width: 100%;
+            height: 100px;
+            border-top: 1px solid #eaeaea;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+          .footer-div {
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+          }
+          .table{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            width: 75%;
+            minHeight: 100vh;
+            overflow: auto;
+          }
+        `}</style>
+
+        <style jsx global>{`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+          }
+
+          * {
+            box-sizing: border-box;
+          }
+        `}</style>
+      </div>
+    );
+  } else {
+    return <div>kurcina</div>;
+  }
 }
