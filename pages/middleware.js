57c6447408c54aeb1160f84282b91cc80ee0a051
popay97 <5880963+popay97@users.noreply.github.com>
2022-06-28 22:48:14 +0200

    ..

--- pages/middleware.js
+++ pages/middleware.js
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
-
-const connectDB = handler => async (req, res) => {
+/* const connectDB = handler => async (req, res) => {
   if (mongoose.connections[0].readyState) {
     // Use current db connection
     return handler(req, res);
@@ -11,4 +10,16 @@ const connectDB = handler => async (req, res) => {
   return handler(req, res);
 };
 
-export default connectDB;
+export default connectDB; */
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request) {
+  if (mongoose.connections[0].readyState) {
+    // Use current db connection
+    return Response("db connected");
+  }
+  // Use new db connection
+  await mongoose.connect(process.env.MONGODB_URI, {});
+  console.log('Connected to MongoDB');
+  return Response("db connected");
+  }
